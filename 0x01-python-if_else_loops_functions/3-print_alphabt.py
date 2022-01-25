12cd2700a03050a339d3b1a2ebb143cb12d8304b
threadsv1 <majdimed3@gmail.com>
2022-01-25 16:40:43 +0100

    edited

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alpha_letters in range(97, 122):
-    if alpha_letters == ord(e) or alpha_letters == ord(q):
-        continue
+if alpha_letters == ord(e) or alpha_letters == ord(q):
+continue
     print("{:c}".format(alpha_letters), end="")
