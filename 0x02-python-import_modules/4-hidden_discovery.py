09f8b3ddc37db321818669e78bb4f0f69eeec972
threadsv1 <majdimed3@gmail.com>
2022-02-02 12:55:26 +0100

    edited

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    len = dir(hidden_4)
+    for i in range (len(len))
+        if(len[i][0] != '_'):
+            print("{}".format(len[i]))
