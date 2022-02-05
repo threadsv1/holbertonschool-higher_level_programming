0571fbda1380637872948626da78ecb48137ce7e
threadsv1 <majdimed3@gmail.com>
2022-02-05 14:06:33 +0100

    edited

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    leng = dir(hidden_4)
-    for i in range (len(leng))
-        if(leng[i][0] != '_'):
-            print("{}".format(leng[i]))
+    length = dir(hidden_4)
+    for i in range(len(length)):
+        if(length[i][0] != '_'):
+            print("{}".format(length[i]))
