d867ffa9d89feac56fe2478c8e8346eb409a993e
threadsv1 <majdimed3@gmail.com>
2022-02-06 02:10:02 +0100

    edited

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
-    return("".join(listofchars))
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for x in range(len(matrix[i])):
+            if x != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[i][x]), end="")
+        print()
