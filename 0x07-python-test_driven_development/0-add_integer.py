8a539c2a9b2b0df9d1031babd77064810359951e
threadsv1 <majdimed3@gmail.com>
2022-03-04 02:30:28 +0100

    edited

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+
 """
-Integers Addition Module
+Add int of float module
 """
 
 
 def add_integer(a, b=98):
-"""
- adds 2 integers
-"""
+    """
+    a function to add integers
+    Args:
+        a(int/float): first int
+        b(int/float): second int
+    Raises:
+        TypeError: when a or b is not an int or a float
+    Return:
+        integer
+    """
     if type(a) not in [int, float]:
-        raise TypeError("a must be an integr")
+        raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     if (a + b) == float('inf') or (a + b) == -float('inf'):
