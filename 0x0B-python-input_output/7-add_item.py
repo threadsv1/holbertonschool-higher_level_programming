95cb8e7ac585d85c31aeccb5bb7aed6c78087708
threadsv1 <majdimed3@gmail.com>
2022-03-28 05:12:43 +0100

    first commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Load Add Save Module"""
+
+
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+args = sys.argv[1:]
+
+if __name__ == '__main__':
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(args)
+    save_to_json_file(my_list, filename)
