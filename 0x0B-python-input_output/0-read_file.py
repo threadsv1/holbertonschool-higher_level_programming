4e04b799fed4092d9c482c1ad2a4e36d633a8787
threadsv1 <majdimed3@gmail.com>
2022-03-28 03:52:06 +0100

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ read file fnc """
 
+
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout """
     with open(filename, 'r', encoding='utf-8') as f:
