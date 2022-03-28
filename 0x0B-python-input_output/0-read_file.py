e73302a16ee04be2ee959a9deb4a106f6f534913
threadsv1 <majdimed3@gmail.com>
2022-03-28 03:47:02 +0100

    second commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" reads a text file (UTF8) and prints it to stdout """
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
