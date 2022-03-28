bfb30cad1bd2cb5d8c13ae586050fa0cf2f7af21
threadsv1 <majdimed3@gmail.com>
2022-03-28 05:24:54 +0100

    fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-"""Load Add Save Module"""
+"""class to json"""
 
 
-import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-filename = "add_item.json"
-args = sys.argv[1:]
-
-if __name__ == '__main__':
-    try:
-        my_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        my_list = []
-
-    my_list.extend(args)
-    save_to_json_file(my_list, filename)
+def class_to_json(obj):
+    """class to json method"""
+    return obj.__dict__
