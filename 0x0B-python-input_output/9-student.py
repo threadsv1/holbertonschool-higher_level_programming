651dacaec4e8f7b7249b704f9d02eaa837393178
threadsv1 <majdimed3@gmail.com>
2022-03-28 05:28:42 +0100

    first commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """class initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """class to json representation"""
+        return (self.__dict__)
