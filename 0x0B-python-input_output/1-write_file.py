30c740a14b58a3bba589bb61cf3f7360a0e50586
threadsv1 <majdimed3@gmail.com>
2022-03-28 03:59:22 +0100

    second commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" number of lines function """
+"""number of lines function"""
 
 
 def number_of_lines(filename=""):
-    """ returns the number of lines of a text file """
+    """returns the number of lines of a text file"""
     with open(filename, 'r', encoding='utf8') as f:
         i = 0
         for line in f:
