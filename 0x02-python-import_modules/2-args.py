0944c9435fc4d2afe81da2c730825eae0f518afe
threadsv1 <majdimed3@gmail.com>
2022-03-30 06:40:33 +0100

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif num == 2:
         print("{:d} arguments:".format(num - 1))
     else:
-        print("{:d} arguments:".format(num - 1))
+        print("{:d} arguments:".format(num))
     for i in range(1, num):
         print("{:d}: {:s}".format(i, sys.argv[i]))
