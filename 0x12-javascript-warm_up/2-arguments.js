328fbbf8d00cc79a5509e58b247fdad991fb3084
threadsv1 <majdimed3@gmail.com>
2022-04-06 14:42:10 +0100

    edited

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) {
+const lengt  = process.argv.length;
+if (lengt  === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (lengt  === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
