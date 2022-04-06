e38945eddab3eb6a67670a7d2218c0ab1ef682d5
threadsv1 <majdimed3@gmail.com>
2022-04-06 21:32:31 +0100

    hot fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle.js');
+
 module.exports = class Square extends Rectangle {
   constructor (size) {
-	  this.height;
-	  this.width;
-	  super(size, size);
+    super(size, size);
   }
 };
