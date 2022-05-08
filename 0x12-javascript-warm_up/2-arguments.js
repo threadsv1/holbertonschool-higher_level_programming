2f568ea78e09d184ad735d046e87a04d780cf0a6
threadsv1 <majdimed3@gmail.com>
2022-05-08 17:49:55 +0100

    new file

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const lengt  = process.argv.length;
-if (lengt  === 2) {
-  console.log('No argument');
-} else if (lengt  === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
