df2c03c63d6d0126ce9713ae4cd06578b9ed31e3
threadsv1 <majdimed3@gmail.com>
2022-05-17 23:37:13 +0100

    updated

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-const request = require('request');
+
+const r = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+
+r.get(process.argv[2], (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) console.log(err);
+    });
   }
 });
