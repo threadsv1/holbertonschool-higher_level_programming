96f6ceef0d3c7c915436523fcee969c671b90cc1
threadsv1 <majdimed3@gmail.com>
2022-05-20 08:59:20 +0100

    newF

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
+    with request.urlopen(req) as r:
+        print(r.headers.get('X-Request-Id'))
